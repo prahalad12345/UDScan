48128c1c7a0621cb8fdaf2aab3096114890e4f75
prahalad12345 <prahaladvijaykumar@gmail.com>
2021-09-12 09:22:19 +0530

    initialize

--- handler/ocrhandler.cpp
+++
@@ -1,9 +0,0 @@
-#include "OCRHandler"
-
-void OCRHandler::setnexthandler(OCRHandler *nexthandler){
-    this->nexthandler = nexthandler; 
-}
-
-OCRHandler *OCRHandler::getNextHandler(){
-    return this->nexthandler;
-}
--- handler/ocrhandler.h
+++
@@ -1,8 +0,0 @@
-class OCRHandler{
-    protected:
-        OCRHandler *nexthandler;
-    public:
-        virtual void setnexthandler(OCRHandler* nexthandler);
-        virtual OCRHandler* getNextHandler();
-        virtual void transform(OCRData* ocrdata)=0;
-}
